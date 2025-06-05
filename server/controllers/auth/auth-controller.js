e4868419cc1e08903089efde8cff5c76273714e8
Pranav Garg <pranavgarg502@gmail.com>
2025-06-05 18:58:21 +0530

    fix

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -101,9 +101,9 @@ const logoutUser = async(req,res) => {
         })
     }
 }
-const authMiddleWare = async(req,res,next) =>{
+const authMiddleWare = (req,res,next) =>{
     const authHeader  = req.headers['authorization'];
-    const token  = authHeader&&  authHeader.split(' ')[1];
+    const token  = authHeader &&  authHeader.split(' ')[1];
     if(!token){
         return res.status(401).json({
             success : false,
