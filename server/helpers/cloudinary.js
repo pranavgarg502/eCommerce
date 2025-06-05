a04d3f60d6f52fdf0fea3fb6b05e1c561f651c11
Pranav Garg <pranavgarg502@gmail.com>
2025-06-05 17:38:33 +0530

    env fix

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -4,8 +4,8 @@ const multer = require('multer');
     // Configuration
     cloudinary.config({ 
         cloud_name:process.env.CLOUDINARY_CLOUD_NAME , 
-        api_key:  CLOUDINARY_API_KEY,
-        api_secret: CLOUDINARY_API_SECRET
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET
     });
     const storage = new multer.memoryStorage();
     async function imageUploadUtil(file){
